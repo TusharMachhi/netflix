fd71a2268459e04c518d59d510f59a20c0f32ffe
tushar machhi <mactushar25@gmail.com>
2022-10-02 09:38:35 +0530

    adding heroku url

--- backend/frontend/src/utils/axiosInstance.js
+++ backend/frontend/src/utils/axiosInstance.js
@@ -2,7 +2,7 @@ import axios from "axios";
 
 
 export const api = axios.create({
-  baseURL: "http://localhost:4000",
+  baseURL: "https://tusharnetflix.herokuapp.com",
 });
 
 const getMovies = async () => {
