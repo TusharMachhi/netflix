3be1ca561c282ab79c63df4b4810f5724a2fab24
tushar machhi <mactushar25@gmail.com>
2022-10-02 08:46:50 +0530

    fixing cors error

--- backend/src/app.js
+++ backend/src/app.js
@@ -20,7 +20,7 @@ const __dirname = path.dirname(__filename);
 
 
 const app = express();
-app.use(cors({origin:"http://localhost:3000",credentials:true}))
+app.use(cors({origin:"https://tusharnetflix.herokuapp.com",credentials:true}))
 app.use(express.urlencoded({extended:true}))
 app.use(express.json())
 app.use(cookieParser())
